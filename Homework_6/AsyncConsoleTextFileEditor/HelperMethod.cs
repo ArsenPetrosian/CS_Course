2a2dec0a652655bf0f74d64ebf0430add92201a3
Arsen <ars.petrosian03@gmail.com>
2024-02-28 16:55:27 +0400

    Added Homework_6

---
+++ Homework_6/AsyncConsoleTextFileEditor/HelperMethod.cs
@@ -0,0 +1,61 @@
+﻿namespace AsyncConsoleTextFileEditor
+{
+    public class HelperMethod
+    {
+        private SemaphoreSlim semaphore = new SemaphoreSlim(1, 1);
+
+        public async Task EditTextFileAsync()
+        {
+            Console.Write("Enter the path of the text file: ");
+            string? filePath = Console.ReadLine();
+
+            if (!File.Exists(filePath))
+            {
+                Console.WriteLine("File does not exist.");
+                return;
+            }
+            try
+            {
+                await semaphore.WaitAsync();
+
+                using (StreamReader reader = new StreamReader(filePath))
+                {
+                    string content = await reader.ReadToEndAsync();
+                    Console.WriteLine($"Content of {filePath}:");
+                    Console.WriteLine(content);
+                }
+
+                Console.WriteLine("Enter new content");
+
+                List<string> lines = new List<string>();
+
+                string? line;
+
+                while((line = Console.ReadLine()) != null)
+                {
+                    lines.Add(line);
+                }
+
+                await semaphore.WaitAsync();
+
+                using (StreamWriter writer = new StreamWriter(filePath, false))
+                {
+                    foreach(string l in lines)
+                    {
+                        await writer.WriteLineAsync(l);
+                    }
+                }
+
+                Console.WriteLine($"File {filePath} has been updated successfully.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"An error occurred: {e.Message}");
+            }
+            finally
+            {
+                semaphore.Release();
+            }
+        }
+    }
+}
---
+++ Homework_6/AsyncConsoleTextFileEditor/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace AsyncConsoleTextFileEditor
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Asynchronous Console Text File Editor");
+
+            HelperMethod helperMethod = new HelperMethod();
+            bool isExited = false;
+
+            while (true)
+            {
+                Console.WriteLine("\nMenu:");
+                Console.WriteLine("1. Edit a text file");
+                Console.WriteLine("2. Exit");
+
+                Console.Write("Enter your choice: ");
+                string? choice = Console.ReadLine();
+
+                switch(choice)
+                {
+                    case "1":
+                        await helperMethod.EditTextFileAsync();
+                        break;
+                    case "2":
+                        Console.WriteLine("Exiting");
+                        isExited = true;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again");
+                        break;
+                }
+
+                if (isExited)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
